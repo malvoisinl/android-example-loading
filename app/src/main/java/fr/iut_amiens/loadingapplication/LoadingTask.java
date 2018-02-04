22ab41dae41c2e947fba31c565ed7a6aef43fa3d
sowdowdow <d.simon_97400@hotmail.fr>
2018-02-04 19:16:33 +0100

    Changed Time display to use LocalTime :anger:
    API required is higher than the API indicated in Gradle app :-1:

--- app/src/main/java/fr/iut_amiens/loadingapplication/LoadingTask.java
+++ app/src/main/java/fr/iut_amiens/loadingapplication/LoadingTask.java
@@ -1,38 +1,47 @@
 package fr.iut_amiens.loadingapplication;
 
 import android.content.Context;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.AsyncTask;
-import android.os.Parcel;
-import android.os.VibrationEffect;
+import android.support.design.widget.FloatingActionButton;
 import android.util.Log;
-import android.widget.Button;
-import android.widget.NumberPicker;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.time.LocalTime;
 
 public class LoadingTask extends AsyncTask<Object, Integer, String> {
 
     private final Context context;
     private final TextView textView;
-    private final int seconds;
-    private final Button button;
+    private final LocalTime timer;
+    private final FloatingActionButton floatingActionButton;
 
-    public LoadingTask(Context context, TextView textView, int seconds, Button button) {
+    public LoadingTask(Context context, TextView textView, LocalTime timer, FloatingActionButton floatingActionButton) {
         this.context = context;
         this.textView = textView;
-        this.button = button;
-        this.seconds = seconds;
+        this.timer = timer;
+        this.floatingActionButton = floatingActionButton;
     }
 
     @Override
     protected String doInBackground(Object[] params) {
         try {
-            int progress = seconds;
-            Log.d("", "======================= doInBackground: "+progress);
-            while (progress > 0) {
+            //int progress = timer.getSecond()+timer.getMinute()*60;
+            int minutes = timer.getMinute();
+            int seconds = timer.getSecond();
+            Log.d("", "======================= doInBackground: "+timer.toString());
+            while (minutes*60+seconds > 0) {
                 Thread.sleep(1000);
-                progress--;
-                publishProgress(progress);
+                if (seconds <= 0){
+                    minutes--;
+                    seconds = 59;
+                }
+                else {
+                    seconds--;
+                }
+                publishProgress(minutes, seconds);
             }
         } catch (InterruptedException e) {
             e.printStackTrace();
@@ -42,24 +51,27 @@ public class LoadingTask extends AsyncTask<Object, Integer, String> {
 
     @Override
     protected void onPreExecute() {
-        textView.setText(seconds+"");
+        textView.setText(timer.toString()+"");
     }
 
     @Override
     protected void onPostExecute(String s) {
-        textView.setText("0");
-        button.setText("Start");
+        textView.setText(timer.toString());
+        floatingActionButton.setBackgroundTintList(ColorStateList.valueOf(Color.GREEN));
         Toast.makeText(context, s, Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onProgressUpdate(Integer... values) {
-        textView.setText(values[0].toString());
+
+        //textView.setText(values[0].toString());
+        textView.setText(LocalTime.of(0,values[0], values[1]).toString());
     }
 
     @Override
     protected void onCancelled() {
         Toast.makeText(context, "annulé", Toast.LENGTH_SHORT).show();
-        textView.setText("0");
+        floatingActionButton.setBackgroundTintList(ColorStateList.valueOf(Color.GREEN));
+        textView.setText(timer.toString());
     }
 }
--- app/src/main/java/fr/iut_amiens/loadingapplication/MainActivity.java
+++ app/src/main/java/fr/iut_amiens/loadingapplication/MainActivity.java
@@ -1,18 +1,24 @@
 package fr.iut_amiens.loadingapplication;
 
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.NumberPicker;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+import java.time.LocalTime;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, NumberPicker.OnValueChangeListener {
 
     private NumberPicker numberPickerSeconds, numberPickerMinutes;
     private TextView textView;
-    private Button button;
+    private FloatingActionButton floatingActionButton;
     private LoadingTask loadingTask = null;
+    private LocalTime timer = LocalTime.of(0,0,0);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,15 +27,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         numberPickerSeconds = findViewById(R.id.numberPickerSecond);
         numberPickerMinutes = findViewById(R.id.numberPickerMinutes);
-        button = findViewById(R.id.button);
+        floatingActionButton = findViewById(R.id.floatingActionButton);
         textView = findViewById(R.id.timeLeft);
 
         numberPickerSeconds.setMinValue(0);
-        numberPickerSeconds.setMaxValue(60);
+        numberPickerSeconds.setMaxValue(59);
         numberPickerMinutes.setMinValue(0);
-        numberPickerMinutes.setMaxValue(60);
-
-        button.setOnClickListener(this);
+        numberPickerMinutes.setMaxValue(59);
+        floatingActionButton.setOnClickListener(this);
+        //if pickers values changes, then update the counter
+        numberPickerSeconds.setOnValueChangedListener(this);
+        numberPickerMinutes.setOnValueChangedListener(this);
     }
 
     @Override
@@ -37,12 +45,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if (loadingTask != null) {
             loadingTask.cancel(true);
             loadingTask = null;
-            button.setText(R.string.start);
+            floatingActionButton.setBackgroundTintList(ColorStateList.valueOf(Color.GREEN));
         } else {
-            int totalSeconds = numberPickerSeconds.getValue() + numberPickerMinutes.getValue()*60;
-            loadingTask = new LoadingTask(this, textView, totalSeconds, button);
+            timer = LocalTime.of(0, numberPickerMinutes.getValue(),numberPickerSeconds.getValue());
+            Log.d("===================", "doInBackground: "+timer.toString());
+            loadingTask = new LoadingTask(this, textView, timer, floatingActionButton);
             loadingTask.execute();
-            button.setText(R.string.cancel);
+            floatingActionButton.setBackgroundTintList(ColorStateList.valueOf(Color.RED));
         }
+    }
+
+    @Override
+    public void onValueChange(NumberPicker numberPicker, int i, int i1) {
+        textView.setText(LocalTime.of(0, numberPickerMinutes.getValue(),numberPickerSeconds.getValue()).toString());
     }
 }
